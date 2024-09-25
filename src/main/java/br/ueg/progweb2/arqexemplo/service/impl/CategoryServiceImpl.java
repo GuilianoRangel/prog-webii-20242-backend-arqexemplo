e8fcd5354db16c779dfeaa49c6eea81eab48e1d3
Guiliano Rangel <guiliano@gmail.com>
2024-09-24 21:06:16 -0300

    [progwebii-11] fix - tratamento campo maiúsculo CategoryServiceImpl

--- src/main/java/br/ueg/progweb2/arqexemplo/service/impl/CategoryServiceImpl.java
+++ src/main/java/br/ueg/progweb2/arqexemplo/service/impl/CategoryServiceImpl.java
@@ -6,12 +6,16 @@ import br.ueg.progweb2.arqexemplo.service.CategoryService;
 import br.ueg.progweb2.arquitetura.service.impl.GenericCrudWithValidationsService;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 public class CategoryServiceImpl extends GenericCrudWithValidationsService<Category, Long, CategoryRepository> implements CategoryService {
     @Override
     protected void prepareToCreate(Category dado) {
         dado.setId(null);
-        dado.setName(dado.getName().toUpperCase());
+        if(Objects.nonNull(dado.getName())) {
+            dado.setName(dado.getName().toUpperCase());
+        }
     }
 
 }
