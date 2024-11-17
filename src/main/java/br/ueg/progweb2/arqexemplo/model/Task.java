cb1fc05da0f534828a2f7d34444d0b2cef5d40ae
Guiliano Rangel <guiliano@gmail.com>
2024-11-16 22:25:53 -0300

    [progwebii-30] ADD - Exemplo de uso de Pesquisa Generic com JpaSpecificationExecutor

--- src/main/java/br/ueg/progweb2/arqexemplo/model/Task.java
+++ src/main/java/br/ueg/progweb2/arqexemplo/model/Task.java
@@ -1,6 +1,8 @@
 package br.ueg.progweb2.arqexemplo.model;
 
+import br.ueg.progweb2.arquitetura.interfaces.ISearchFieldData;
 import br.ueg.progweb2.arquitetura.model.GenericModel;
+import br.ueg.progweb2.arquitetura.model.annotation.Searchable;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -18,7 +20,7 @@ import static jakarta.persistence.GenerationType.SEQUENCE;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "TASK")
-public  class Task implements GenericModel<Long> {
+public  class Task implements GenericModel<Long>, ISearchFieldData<Long> {
 
     @Id
     @SequenceGenerator(
@@ -31,19 +33,29 @@ public  class Task implements GenericModel<Long> {
             generator = "task_sequence"
     )
     @Column(name = "id", nullable = false)
+    @Searchable(label="Código")
     private Long id;
 
     @NotNull
     @Column(name = "descricao",  nullable = false, length = 200)
+    @Searchable(label = "Descrição")
     private String description;
 
     @Column(name = "concluida")
+    @Searchable(label = "Concluída")
     private Boolean completed;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "categoria_id", referencedColumnName = "id", nullable = false, foreignKey = @ForeignKey(name = "fk_category_tipo"))
+    @Searchable(label="Categória", listEntityValues = true)
     private Category category;
 
     @Column(name = "data_final")
+    @Searchable(label = "Dta. Final")
     private LocalDate dueDate;
+
+    @Override
+    public String getSearchDescription() {
+        return this.getId()+" - "+this.getDescription();
+    }
 }
